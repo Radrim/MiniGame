0d8b73dd4841c6eb26a2b16c9809ec4c8afb05cd
Radrim <gabdrahmanov.radmir2018@gmail.com>
2021-06-04 13:31:50 +0300

    Mini_Games

--- src/main/java/com/example/myapplication/InfoActivity.java
+++ src/main/java/com/example/myapplication/InfoActivity.java
@@ -2,9 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -14,5 +17,15 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Button btn_back3 = (Button)findViewById(R.id.btn_back3);
+        btn_back3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(InfoActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }catch (Exception e){}
+            }
+        });
     }
 }
--- src/main/java/com/example/myapplication/SettingActivity.java
+++ src/main/java/com/example/myapplication/SettingActivity.java
@@ -2,9 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -14,5 +17,15 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Button btn_back2 = (Button)findViewById(R.id.btn_back2);
+        btn_back2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(SettingActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }catch (Exception e){}
+            }
+        });
     }
 }
